e132beb3f081f94798e69f6333cb72ef05d0d055
vash68 <cppwiz@yahoo.com>
2020-03-10 16:53:30 -0600

    Added execution branch, see UtilService.subtract()

--- module1/src/main/java/org/example/service/UtilService.java
+++ module1/src/main/java/org/example/service/UtilService.java
@@ -6,15 +6,23 @@ public class UtilService {
     public int add(int a, int b) {
         //
         System.out.println("Doing addition!");
-        ;
         return a + b;
     }
 
 
     public int subtract(int a, int b) {
         System.out.println("Doing subtraction!");
+        if(a ==b) {
+            //dangerous branch
+            return 0;
+        } else {
+            return a - b;
+        }
+    }
 
-        return a - b;
+
+    public void noOp(int a, int b) {
+        System.out.println("Doing nothing!");
     }
 
 
